50e8a4177356e7b05eac1dfc4406f6430ad9f675
Lisandro <87555178+LISi-cmd-coder@users.noreply.github.com>
2024-10-03 11:34:38 -0300

    Add files via upload

---
+++ ITEM2/Empresa.java
@@ -0,0 +1,8 @@
+public class Empresa
+{    
+    public static void main(String []args )
+    {
+        Empleado empleado1= new Empleado(123,"Benitez","Raul",2500,2014,44023123,2000);
+        empleado1.mostrar();
+    }
+} 
